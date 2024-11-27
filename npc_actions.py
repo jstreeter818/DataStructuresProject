35a894ae2a4c91a949f9bb09363d300f63a4a3cc
Judas Streeter <131019325+jstreeter818@users.noreply.github.com>
2024-11-27 17:00:12 -0600

    Delete npc_actions.py

--- npc_actions.py
+++
@@ -1,8 +0,0 @@
-def handshake(character):
-    print("*" + character.name + " shakes your hand*")
-
-def give_map():
-    from game_objects import player, map
-    player.inventory.append(map)
-    map.picked_up = True
-    print("*Duncan hands you a map of the museum, and you place it in your inventory*")
